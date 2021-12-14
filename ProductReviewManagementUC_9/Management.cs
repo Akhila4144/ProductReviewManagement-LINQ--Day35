9fcbee1671e8a8ded512b3eb50cd876a776311df
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-14 21:49:53 +0530

    UC_9 DOne

---
+++ ProductReviewManagementUC_9/Management.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementUC_9
+{
+    public class Management
+    {
+        public void RetrieveAverage(List<ProductReview> review)
+        {
+           
+            var recordData = (review.GroupBy(p => p.ProductId).Select(x => new { ProductId = x.Key, Avg = x.Average(p => p.Rating) }));
+
+            foreach (var list in recordData)
+            {
+              
+                Console.WriteLine("Product Id : " + list.ProductId + " || Average : " + list.Avg);
+            }
+        }
+    }
+}
---
+++ ProductReviewManagementUC_9/ProductReview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagementUC_9
+{
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+
+        public int UserId { get; set; }
+
+        public double Rating { get; set; }
+
+        public string Review { get; set; }
+
+        public bool isLike { get; set; }
+    }
+}
+
+
