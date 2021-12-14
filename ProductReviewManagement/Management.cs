ce30b5734716005df1e160a5ecd9424710950a4f
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-14 21:43:40 +0530

    UC7 Done

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public void RetrieveProductIdAndReviewUsingSelect(List<ProductReview> review)
+        {
+            var recordData = review.Select(products => new { ProductId = products.ProductId, Review = products.Review });
+
+            foreach (var list in recordData)
+            {
+                                Console.WriteLine("Product Id : " + list.ProductId + " || Review : " + list.Review);
+            }
+        }
+    }
+
+}
+
