64f7bfc81aac1cc19c5f6e30feaffdb5872c4f71
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-14 21:10:26 +0530

    UC1 to UC4 Done By Using Linq

---
+++ ProductReviewManagement_UC1(Linq)/ProductReview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement_UC1_Linq_
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+
+        public int UserID { get; set; }
+        
+        public double Rating { get; set; }
+        
+        public string Review { get; set; }
+        
+        public bool isLike { get; set; }
+    }
+}
